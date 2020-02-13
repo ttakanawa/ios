d1497b7a3d290999a37be90c71823ee4b6934070
Ricardo Sanchez <odrakir@gmail.com>
2020-02-13 12:24:09 +0100

    Make app iOS <13 compatible

--- Toggl-iOS/Toggl-iOS/AppDelegate.swift
+++ Toggl-iOS/Toggl-iOS/AppDelegate.swift
@@ -9,29 +9,56 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    var window: UIWindow?
 
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool
+    {
         return true
+    }
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication)
+    {
+        // Not called under iOS 13 - See SceneDelegate sceneWillResignActive
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication)
+    {
+        // Not called under iOS 13 - See SceneDelegate sceneDidEnterBackground
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication)
+    {
+        // Not called under iOS 13 - See SceneDelegate sceneWillEnterForeground
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication)
+    {
+        // Not called under iOS 13 - See SceneDelegate sceneDidBecomeActive
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration
+    {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>)
+    {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
