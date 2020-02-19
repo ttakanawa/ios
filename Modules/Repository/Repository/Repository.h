485150281dede05a592e8ee6b8e4dd190d591be2
Ricardo Sanchez <odrakir@gmail.com>
2020-02-19 12:37:51 +0100

    Add repository

---
+++ Modules/Repository/Repository/Repository.h
@@ -0,0 +1,19 @@
+//
+//  Repository.h
+//  Repository
+//
+//  Created by Ricardo Sánchez Sotres on 19/02/2020.
+//  Copyright © 2020 Ricardo Sánchez Sotres. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Repository.
+FOUNDATION_EXPORT double RepositoryVersionNumber;
+
+//! Project version string for Repository.
+FOUNDATION_EXPORT const unsigned char RepositoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Repository/PublicHeader.h>
+
+
---
+++ Modules/Repository/Repository/Repository.swift
@@ -0,0 +1,14 @@
+//
+//  Repository.swift
+//  Repository
+//
+//  Created by Ricardo Sánchez Sotres on 19/02/2020.
+//  Copyright © 2020 Ricardo Sánchez Sotres. All rights reserved.
+//
+
+import Foundation
+
+class Repository
+{
+    
+}
