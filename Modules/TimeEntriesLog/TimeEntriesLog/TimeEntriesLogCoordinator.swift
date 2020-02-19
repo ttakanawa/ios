74af9cc6cf15e0d4c63e4c51cf55c14a205746f1
Ricardo Sanchez <odrakir@gmail.com>
2020-02-19 12:31:14 +0100

    Add time entries log

---
+++ Modules/TimeEntriesLog/TimeEntriesLog/TimeEntriesLogCoordinator.swift
@@ -0,0 +1,7 @@
+//
+//  TimeEntriesLogCoordinator.swift
+//  TimeEntriesLog
+//
+//  Created by Ricardo Sánchez Sotres on 19/02/2020.
+//  Copyright © 2020 Ricardo Sánchez Sotres. All rights reserved.
+//
