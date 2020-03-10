0a87efdc7c9e87eb64ffcc3eecc8aaea9bff7699
Ricardo Sanchez <odrakir@gmail.com>
2020-03-10 10:29:06 +0100

    Add timeEntryViewModelsSelector

--- Modules/Timer/Timer/TimeEntriesLog/TimeEntriesSelector.swift
+++ Modules/Timer/Timer/TimeEntriesLog/TimeEntriesSelector.swift
@@ -3,26 +3,8 @@ import Models
 let timeEntriesSelector: (TimeEntriesLogState) -> [DayViewModel] = { state in
     
     guard case .loaded(_) = state.entities.loading else { return [] }
-    return state.entities.timeEntries.values
+    return timeEntryViewModelsSelector(state.entities)
         .sorted(by: { $0.start > $1.start })
-        .compactMap({ timeEntry in
-            guard let workspace = state.entities.getWorkspace(timeEntry.workspaceId) else {
-                //fatalError("Workspace missing")
-                //TODO This shouldn't happen, what should we do here?
-                return nil
-            }
-            
-            let project = state.entities.getProject(timeEntry.projectId)
-            
-            return TimeEntryViewModel(
-                timeEntry: timeEntry,
-                workspace: workspace,
-                project: project,
-                client: state.entities.getClient(project?.clientId),
-                task: state.entities.getTask(timeEntry.taskId),
-                tags: timeEntry.tagIds?.compactMap(state.entities.getTag)
-            )
-        })
         .grouped(by: { $0.start.ignoreTimeComponents() })
         .map(DayViewModel.init)
         .sorted(by: { $0.day > $1.day })
